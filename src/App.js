04bbe9f2c5b7405a66ffa77c8bf1f60afa215ed1
Oudani Achraf <achraf.oudani@gmail.com>
2021-02-14 23:42:38 +0100

    useDispatch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Login from "./Login";
-import { selectUser } from "./features/userSlice";
+import { login, logout, selectUser } from "./features/userSlice";
 import Feed from "./Feed";
 import Header from "./Header";
 import SideBar from "./SideBar";
+import { auth } from "./firebase";
 
 function App() {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // user is logged in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        );
+      } else {
+        // user is logged out
+        dispatch(logout());
+      }
+    });
+  }, []);
   return (
     <div className="App">
       <Header />
