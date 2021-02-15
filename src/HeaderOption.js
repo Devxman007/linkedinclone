13965da64ede3cd2066546b247c35dffdae4a96e
Oudani Achraf <achraf.oudani@gmail.com>
2021-02-16 00:13:51 +0100

    add signIn function

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -2,9 +2,9 @@ import { Avatar } from "@material-ui/core";
 import React from "react";
 import "./HeaderOption.css";
 
-function HeaderOption({ avatar, Icon, title }) {
+function HeaderOption({ avatar, Icon, title, onclick }) {
   return (
-    <div className="headerOption">
+    <div onClick={onclick} className="headerOption">
       {Icon && <Icon className="headerOption__icon" />}
       {avatar && <Avatar className="headerOption__icon" src={avatar} />}
       <h3 className="headerOption__title">{title}</h3>
--- src/Login.js
+++ src/Login.js
@@ -11,6 +11,19 @@ function Login() {
   const dispatch = useDispatch();
   const loginToApp = (e) => {
     e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        dispatch(
+          login({
+            email: userAuth.user.email,
+            uid: userAuth.user.uid,
+            displayName: userAuth.user.displayName,
+            photoURL: userAuth.user.profilePic,
+          })
+        );
+      })
+      .catch((error) => alert(error));
   };
   const register = () => {
     if (!name) {
